8d86ddc3839ec55a58aba229af658d5aab6177cd
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-07-21 21:12:27 +0530

    Part 27 Lets Play First Song

--- app/src/main/java/com/createsapp/musicplayer/Adapter/MusicAdapter.kt
+++ app/src/main/java/com/createsapp/musicplayer/Adapter/MusicAdapter.kt
@@ -36,6 +36,8 @@ class MusicAdapter(private val context: Context, private val musicList: ArrayLis
             .into(holder.image)
         holder.root.setOnClickListener {
             val intent = Intent(context, PlayerActivity::class.java)
+            intent.putExtra("index", position)
+            intent.putExtra("class", "MusicAdapter")
             ContextCompat.startActivity(context, intent, null)
         }
     }
--- app/src/main/java/com/createsapp/musicplayer/PlayerActivity.kt
+++ app/src/main/java/com/createsapp/musicplayer/PlayerActivity.kt
@@ -1,12 +1,21 @@
 package com.createsapp.musicplayer
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.createsapp.musicplayer.databinding.ActivityPlayerBinding
+import com.createsapp.musicplayer.model.Music
 
 class PlayerActivity : AppCompatActivity() {
 
+    companion object{
+       lateinit var musicListPA: ArrayList<Music>
+       var songPosition:Int = 0
+        var mediaPlayer:MediaPlayer? = null
+    }
+
     private lateinit var binding: ActivityPlayerBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +23,18 @@ class PlayerActivity : AppCompatActivity() {
         binding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+       songPosition = intent.getIntExtra("index", 0)
+        when(intent.getStringExtra("class")){
+            "MusicAdapter" -> {
+                musicListPA = ArrayList()
+                musicListPA.addAll(MainActivity.MusicListMA)
+
+                if (mediaPlayer == null) mediaPlayer = MediaPlayer()
+                mediaPlayer!!.reset()
+                mediaPlayer!!.setDataSource(musicListPA[songPosition].path)
+                mediaPlayer!!.prepare()
+                mediaPlayer!!.start()
+            }
+        }
     }
 }
