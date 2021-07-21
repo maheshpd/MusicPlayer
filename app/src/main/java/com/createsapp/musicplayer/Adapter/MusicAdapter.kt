a328cddced1690fa5e2c61e20bbda494c8abc73f
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-07-21 21:01:38 +0530

    Part 26 Format Song Duration

--- app/src/main/java/com/createsapp/musicplayer/Adapter/MusicAdapter.kt
+++ app/src/main/java/com/createsapp/musicplayer/Adapter/MusicAdapter.kt
@@ -1,14 +1,18 @@
 package com.createsapp.musicplayer.Adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.createsapp.musicplayer.PlayerActivity
 import com.createsapp.musicplayer.R
 import com.createsapp.musicplayer.databinding.MusicViewBinding
 import com.createsapp.musicplayer.model.Music
+import com.createsapp.musicplayer.model.formatDuration
 
 class MusicAdapter(private val context: Context, private val musicList: ArrayList<Music>): RecyclerView.Adapter<MusicAdapter.MyHolder>() {
 
@@ -17,6 +21,7 @@ class MusicAdapter(private val context: Context, private val musicList: ArrayLis
         val album = binding.songAlbum
         val image = binding.imageMV
         val duration = binding.songDuration
+        val root = binding.root
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicAdapter.MyHolder {
@@ -26,9 +31,13 @@ class MusicAdapter(private val context: Context, private val musicList: ArrayLis
     override fun onBindViewHolder(holder: MusicAdapter.MyHolder, position: Int) {
         holder.title.text = musicList[position].title
         holder.album.text = musicList[position].album
-        holder.duration.text = musicList[position].duration.toString()
+        holder.duration.text = formatDuration(musicList[position].duration)
         Glide.with(context).load(musicList[position].artUri).apply(RequestOptions().placeholder(R.drawable.music_player).centerCrop())
             .into(holder.image)
+        holder.root.setOnClickListener {
+            val intent = Intent(context, PlayerActivity::class.java)
+            ContextCompat.startActivity(context, intent, null)
+        }
     }
 
     override fun getItemCount(): Int {
