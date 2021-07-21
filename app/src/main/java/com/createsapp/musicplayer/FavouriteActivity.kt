e12a74cf842c402d3223c7946764dca5ba56a215
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-07-21 08:49:19 +0530

    Part 14 Favorites & Playlists Activity UI

---
+++ app/src/main/java/com/createsapp/musicplayer/FavouriteActivity.kt
@@ -0,0 +1,11 @@
+package com.createsapp.musicplayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FavouriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favourite)
+    }
+}
