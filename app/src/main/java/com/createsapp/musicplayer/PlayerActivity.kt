852fb9c5301ca63bf1d05deeb6ca13766fbfba55
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-07-21 21:30:54 +0530

    Part 28 Play & Pause Button Functionality

--- app/src/main/java/com/createsapp/musicplayer/PlayerActivity.kt
+++ app/src/main/java/com/createsapp/musicplayer/PlayerActivity.kt
@@ -3,6 +3,8 @@ package com.createsapp.musicplayer
 import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.createsapp.musicplayer.databinding.ActivityPlayerBinding
 import com.createsapp.musicplayer.model.Music
 
@@ -12,6 +14,7 @@ class PlayerActivity : AppCompatActivity() {
        lateinit var musicListPA: ArrayList<Music>
        var songPosition:Int = 0
         var mediaPlayer:MediaPlayer? = null
+        var isPlaying:Boolean = false
     }
 
     private lateinit var binding: ActivityPlayerBinding
@@ -22,19 +25,53 @@ class PlayerActivity : AppCompatActivity() {
         setTheme(R.style.coolPink)
         binding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initializeLayout()
+        binding.playPausePA.setOnClickListener {
+            if (isPlaying) pauseMusic()
+            else playMusic()
+        }
+    }
 
-       songPosition = intent.getIntExtra("index", 0)
+    private fun setLayout(){
+        Glide.with(this).load(musicListPA[songPosition].artUri).apply(RequestOptions().placeholder(R.drawable.music_player).centerCrop())
+            .into(binding.songImgPA)
+        binding.songNamePA.text = musicListPA[songPosition].title
+    }
+
+    private fun createMediaPlayer(){
+        try {
+            if (mediaPlayer == null) mediaPlayer = MediaPlayer()
+            mediaPlayer!!.reset()
+            mediaPlayer!!.setDataSource(musicListPA[songPosition].path)
+            mediaPlayer!!.prepare()
+            mediaPlayer!!.start()
+            isPlaying = true
+            binding.playPausePA.setIconResource(R.drawable.pause_icon)
+        } catch (e:Exception){return}
+    }
+
+    private fun initializeLayout(){
+        songPosition = intent.getIntExtra("index", 0)
         when(intent.getStringExtra("class")){
             "MusicAdapter" -> {
                 musicListPA = ArrayList()
                 musicListPA.addAll(MainActivity.MusicListMA)
 
-                if (mediaPlayer == null) mediaPlayer = MediaPlayer()
-                mediaPlayer!!.reset()
-                mediaPlayer!!.setDataSource(musicListPA[songPosition].path)
-                mediaPlayer!!.prepare()
-                mediaPlayer!!.start()
+                setLayout()
+                createMediaPlayer()
             }
         }
     }
+
+    private fun playMusic(){
+        binding.playPausePA.setIconResource(R.drawable.pause_icon)
+        isPlaying = true
+        mediaPlayer!!.start()
+    }
+    private fun pauseMusic(){
+        binding.playPausePA.setIconResource(R.drawable.play_icon)
+        isPlaying = false
+        mediaPlayer!!.pause()
+    }
+
 }
