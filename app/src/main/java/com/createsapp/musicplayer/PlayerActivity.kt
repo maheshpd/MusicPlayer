a60bed931c05055d40b164fc0a0cab38cd2e9258
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-07-22 21:51:56 +0530

    Part 32 Creating MusicService Class

--- app/src/main/java/com/createsapp/musicplayer/PlayerActivity.kt
+++ app/src/main/java/com/createsapp/musicplayer/PlayerActivity.kt
@@ -1,20 +1,26 @@
 package com.createsapp.musicplayer
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.IBinder
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.createsapp.musicplayer.databinding.ActivityPlayerBinding
 import com.createsapp.musicplayer.model.Music
+import com.createsapp.musicplayer.service.MusicService
 
-class PlayerActivity : AppCompatActivity() {
+class PlayerActivity : AppCompatActivity(), ServiceConnection {
 
     companion object{
        lateinit var musicListPA: ArrayList<Music>
        var songPosition:Int = 0
-        var mediaPlayer:MediaPlayer? = null
+//        var mediaPlayer:MediaPlayer? = null
         var isPlaying:Boolean = false
+        var musicService: MusicService? = null
     }
 
     private lateinit var binding: ActivityPlayerBinding
@@ -25,6 +31,12 @@ class PlayerActivity : AppCompatActivity() {
         setTheme(R.style.coolPink)
         binding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //For Starting Service
+        val intent = Intent(this, MusicService::class.java)
+        bindService(intent, this, BIND_AUTO_CREATE)
+        startService(intent)
+
         initializeLayout()
 
         binding.playPausePA.setOnClickListener {
@@ -49,11 +61,11 @@ class PlayerActivity : AppCompatActivity() {
 
     private fun createMediaPlayer(){
         try {
-            if (mediaPlayer == null) mediaPlayer = MediaPlayer()
-            mediaPlayer!!.reset()
-            mediaPlayer!!.setDataSource(musicListPA[songPosition].path)
-            mediaPlayer!!.prepare()
-            mediaPlayer!!.start()
+            if (musicService!!.mediaPlayer == null) musicService!!.mediaPlayer = MediaPlayer()
+            musicService!!.mediaPlayer!!.reset()
+            musicService!!.mediaPlayer!!.setDataSource(musicListPA[songPosition].path)
+            musicService!!.mediaPlayer!!.prepare()
+            musicService!!.mediaPlayer!!.start()
             isPlaying = true
             binding.playPausePA.setIconResource(R.drawable.pause_icon)
         } catch (e:Exception){return}
@@ -82,12 +94,12 @@ class PlayerActivity : AppCompatActivity() {
     private fun playMusic(){
         binding.playPausePA.setIconResource(R.drawable.pause_icon)
         isPlaying = true
-        mediaPlayer!!.start()
+        musicService!!.mediaPlayer!!.start()
     }
     private fun pauseMusic(){
         binding.playPausePA.setIconResource(R.drawable.play_icon)
         isPlaying = false
-        mediaPlayer!!.pause()
+        musicService!!.mediaPlayer!!.pause()
     }
 
     private fun prevNextSong(increment: Boolean) {
@@ -114,6 +126,16 @@ class PlayerActivity : AppCompatActivity() {
                 songPosition = musicListPA.size-1
             else --songPosition
         }
+    }
+
+    override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+        val binder = service as MusicService.MyBinder
+        musicService = binder.currentService()
+        createMediaPlayer()
+    }
+
+    override fun onServiceDisconnected(name: ComponentName?) {
+        musicService = null
     }
 
 }
---
+++ app/src/main/java/com/createsapp/musicplayer/service/MusicService.kt
@@ -0,0 +1,23 @@
+package com.createsapp.musicplayer.service
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Binder
+import android.os.IBinder
+
+class MusicService: Service() {
+
+    private var myBinder = MyBinder()
+    var mediaPlayer:MediaPlayer?=null
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return myBinder
+    }
+
+    inner class MyBinder:Binder(){
+        fun currentService(): MusicService{
+            return this@MusicService
+        }
+    }
+}
