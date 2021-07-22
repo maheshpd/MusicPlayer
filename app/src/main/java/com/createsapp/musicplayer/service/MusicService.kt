0cdcf574a61b3bd6b279afef533e22150065e1ca
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-07-22 22:20:53 +0530

    Part 34 Foreground Service with Notification

--- app/src/main/java/com/createsapp/musicplayer/service/MusicService.kt
+++ app/src/main/java/com/createsapp/musicplayer/service/MusicService.kt
@@ -2,16 +2,24 @@ package com.createsapp.musicplayer.service
 
 import android.app.Service
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.media.MediaPlayer
 import android.os.Binder
 import android.os.IBinder
+import android.support.v4.media.session.MediaSessionCompat
+import androidx.core.app.NotificationCompat
+import com.createsapp.musicplayer.PlayerActivity
+import com.createsapp.musicplayer.R
+import com.createsapp.musicplayer.utils.ApplicationClass
 
 class MusicService: Service() {
 
     private var myBinder = MyBinder()
     var mediaPlayer:MediaPlayer?=null
+    private lateinit var mediaSession: MediaSessionCompat
 
     override fun onBind(intent: Intent?): IBinder? {
+        mediaSession = MediaSessionCompat(baseContext, "My Music")
         return myBinder
     }
 
@@ -19,5 +27,24 @@ class MusicService: Service() {
         fun currentService(): MusicService{
             return this@MusicService
         }
+    }
+
+    fun showNotification(){
+        val notification = NotificationCompat.Builder(baseContext, ApplicationClass.CHANNEL_ID)
+            .setContentTitle(PlayerActivity.musicListPA[PlayerActivity.songPosition].title)
+            .setContentText(PlayerActivity.musicListPA[PlayerActivity.songPosition].artUri)
+            .setSmallIcon(R.drawable.playlist_icon)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.music_player))
+            .setStyle(androidx.media.app.NotificationCompat.MediaStyle().setMediaSession(mediaSession.sessionToken))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setOnlyAlertOnce(true)
+            .addAction(R.drawable.previous_icon, "Previous", null)
+            .addAction(R.drawable.play_icon, "Previous", null)
+            .addAction(R.drawable.next_icon, "Next", null)
+            .addAction(R.drawable.exit_icon, "Exit", null)
+            .build()
+
+        startForeground(13, notification)
     }
 }
